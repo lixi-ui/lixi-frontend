17ba22a3a626217f337dca90d78536da9b4af6f8
niker <lingyp@yisa.com>
2020-07-05 13:27:05 +0800

    add router

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="page-component">
     初始化
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import navConfig from './nav.config.json'
 // import index from '@/pages/index'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+const LOAD_DOCS_MAP = {
+  'zh-CN': path => {
+    return r => require.ensure([], () =>
+      r(require(`../pages/docs${path}/index.md`)),
+    'zh-CN');
+  }
+};
 
-  ]
+function loadDocs(lang,path){
+  return LOAD_DOCS_MAP[lang](path);
+}
+
+var routes = []
+
+navConfig['zh-CN'][0]['groups'][0]['list'].forEach(item => {
+  console.log(item);
+  var obj = {
+    path: item.path,
+    name: "component-" + item.title,
+    meta: "",
+    component: loadDocs('zh-CN', item.path)
+  }
+  routes.push(obj);
+});
+
+export default new Router({
+  mode: 'hash',
+  routes: routes
 })
